1212b189656129f9554ff67656a47d8f1f99c541
Olumide <olumidedaramola426@gmail.com>
2024-10-12 23:13:21 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # proxy_server
 A proxy is an intermediary application which sits between two or more services and processes. It modifies the requests and responses in both directions.
+
+This repo contains bare bone implemetation of proxy server written in nodejs
--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-const express = require('express')
-const morgan =  require('morgan')
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const express = require("express");
+const morgan = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+
+// Configuration
+const PORT = 3000;
+const HOST = "localhost";
+const API_SERVICE_URL = "https://jsonplaceholder.typicode.com";
+
+app.use(morgan("dev"));
+
+app.get("/info", (req, res, next) => {
+  res.send(
+    "This is a proxy service which proxies to Billing and Account APIs."
+  );
+});
+
+// Authorization
+app.use("", (req, res, next) => {
+  if (req.header.autorization) {
+    next();
+  } else {
+    res.sendStatus(403); // forbidden
+  }
+});
+
+// Proxy endpoints
+app.get(
+  "/json_placeholder",
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/json_placeholder`]: "",
+    },
+  })
+);
+
+// proxy start
+app.listen(PORT, HOST, () => {
+    console.log(`Starting Proxy at ${HOST}:${PORT}`)
+})
